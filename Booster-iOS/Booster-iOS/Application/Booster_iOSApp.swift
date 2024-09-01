a0554838f11159941caebd9351da1ded6fd02691
hhhello <hhhello0507@dgsw.hs.kr>
2024-09-01 22:02:20 +0900

    1

---
+++ Booster-iOS/Booster-iOS/Application/Booster_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Booster_iOSApp.swift
+//  Booster-iOS
+//
+//  Created by hhhello0507 on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Booster_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
